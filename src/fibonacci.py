e87921c1ca5e39c3fb302ddc58352233ec802f6d
davidquie12 <56687064+davidquie12@users.noreply.github.com>
2023-12-18 00:07:51 +0100

    async fibo

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -21,16 +21,16 @@ print(list(fib_gen))
 '''
 
 async def taak1(count : int, r : int):
-    print("taak1")
+    print("taak1 :",r,"s")
     await asyncio.sleep(r)
 
 
 async def taak2(count : int, r : int):
-    print("taak2")
+    print("taak2",r,"s")
     await asyncio.sleep(r)
 
 async def taak3(count : int, r : int):
-    print("taak3")
+    print("taak3",r,"s")
     await asyncio.sleep(r)
 
 async def async_fibanocci(num : int):
@@ -38,25 +38,31 @@ async def async_fibanocci(num : int):
     for i in range(num):
         yield num1
         num1, num2 = num2, num1 + num2
+        print(num2)
+        
         await asyncio.sleep(5)
         
+        
 async def async_chain(count : int, num : int, r : list):
-    cu = await taak1(count,r[0])
-    cd = await taak2(count,r[1])
-    cp = await taak3(count,r[2])
-    fib = await async_fibanocci(num)
+    await taak1(count,r[0])
+    await taak2(count,r[1])
+    await taak3(count,r[2])
+    async for fib_num in async_fibanocci(num):
+        pass
+
         
 async def main(*args):
-    res = await asyncio.gather(*(async_chain(count,num,r) for count , num , r in args))    
-    return res
+    await asyncio.gather(*(async_chain(count,num,r) for count , num , r in args))    
+    
     
     
 if __name__ == "__main__":
-    
-    r1 = rand.randint(1,5)
-    r2 = rand.randint(1,5) 
-    r3 = rand.randint(1,5) 
-    
-    rands = [r1,r2,r3]
-    args = [0,10,rands] 
-    asyncio.run(main(args))
+    while True:
+        r1 = rand.randint(1,5)
+        r2 = rand.randint(1,5) 
+        r3 = rand.randint(1,5) 
+        
+        rands = [r1,r2,r3]
+        args = [0,10,rands] 
+        result=asyncio.run(main(args))
+        print(result)
